4c7868afd577b93111433e6d8ef434d927490768
p siva sai <56314427+pallasivasai@users.noreply.github.com>
2026-01-07 20:46:57 +0530

    Add files via upload

---
+++ Contact.jsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <section id="contact" className="section dark">
+      <h1>Contact</h1>
+      <p>LinkedIn: PALLA SIVA SAI</p>
+    </section>
+  );
+}
